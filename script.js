a8c7e072a2a8cfa50d17948cf9ae02a5f98596cb
zrobin88 <zrobin88@gmail.com>
2019-01-15 15:06:49 -0500

    hw submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 
 //array of topics for GIF buttons 
-const topics = ["music", "heavy metal", "guitar", "arnold schwarzenegger", "metallica", "star wars", "trailer park boys", "dimebag darrell","goku", "vegeta"]
+const topics = ["music", "heavy metal", "guitar", "arnold schwarzenegger", "metallica", "star wars", "trailer park boys", "dimebag darrell", "goku", "vegeta"]
 
 
 //add gifs based on value of input 
-$("#add-gif").on("click", function(event) {
+$("#add-gif").on("click", function () {
   event.preventDefault();
 
-  
+
   // This line of code will grab the input from the textbox
-  let userInput = $("#gif-input").val().trim(); 
+  let userInput = $("#gif-input").val().trim();
 
   // The topic from the textbox is then added to our array
   topics.push(userInput);
+
+
 
   // Calling renderButtons 
   renderButtons();
@@ -35,29 +37,38 @@ renderButtons = () => {
     // Added a data-attribute
     a.attr("data-name", topics[i]);
     // Provided the initial button text
+
     a.text(topics[i]);
     // Added the button to the buttons-view div
     $("#buttons-view").append(a);
 
+
+
     console.log(topics[i]);
   }
+  $("#clear").on("click", function () {
+    location.reload();
+  });
 }
 
 
+
 //call renderbuttons function to display buttons
-renderButtons(); 
+renderButtons();
 
 
 
 //create click event for buttons that generate gifs based on the name of each button 
-$(".topics-button").on("click", function () {
+let gifBtn = $(".topics-button").on("click", function () {
+
   
+
   //variable to hold button info (by name) in API key 
   let gifButton = $(this).attr("data-name");
- //var that holds api link and key 
-  let queryURL = "http://api.giphy.com/v1/gifs/search?q="+gifButton+"&api_key=nrQ5ktF2K7HdVctvvKa70hbMfPiKAlo6";
+  //var that holds api link and key 
+  let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifButton + "&api_key=nrQ5ktF2K7HdVctvvKa70hbMfPiKAlo6";
 
- 
+
   //ajax call to api 
   $.ajax({
     url: queryURL,
@@ -65,21 +76,44 @@ $(".topics-button").on("click", function () {
   }).then(function (response) {
     console.log(response)
 
-    for(let i =0; i <10; i++){
-  
-      let imageDiv = $("<img>"); 
+    for (let i = 0; i < 10; i++) {
+
+      let imageDiv = $("<img>");
       imageDiv.addClass("img");
-      imageDiv.attr("src",response.data[i].images.fixed_width.url);
-      $("#gif-div").append(imageDiv); 
+      imageDiv.attr("src", response.data[i].images.fixed_width.url);
+      $("#gif-div").append(imageDiv);
     }
   });
 
 
 });
 
+//$(document).on instead of $(".movie").on to add event listeners to dynamically generated elements
+$(document).on("click", ".topics-button", function () {
+  //variable to hold button info (by name) in API key 
+  let gifButton = $(this).attr("data-name");
+  //var that holds api link and key 
+  let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifButton + "&api_key=nrQ5ktF2K7HdVctvvKa70hbMfPiKAlo6";
 
 
-$("#clear").on("click", function(){
-  location.reload();
+  //ajax call to api 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+
+    for (let i = 0; i < 10; i++) {
+
+      let imageDiv = $("<img>");
+      imageDiv.addClass("img");
+      imageDiv.attr("src", response.data[i].images.fixed_width.url);
+      $("#gif-div").append(imageDiv);
+    }
+  });
+
 });
+
+
+
 
